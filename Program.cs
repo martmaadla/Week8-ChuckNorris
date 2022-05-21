242509e7ea8e8b739064c6c9c2d2da2fcf49bcc1
Märt Maadla <mart.maadla@reaalprojekt.ee>
2022-05-21 20:27:46 +0300

    Chuck Norris

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\...\samples";
+
+            Console.WriteLine(GetRandomFromFile("chuck.txt", filePath));
+            Console.ReadLine();
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+
+        private static string GetRandomFromFile(string filename, string filePath)
+        {
+            filePath += @"\" + filename;
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+        }
+    }
+}
